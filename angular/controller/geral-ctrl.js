e9219fa81e18cabc6c021268e62249634fd357ac
6ust <gust.guedes95@gmail.com>
2019-10-19 17:27:06 -0300

    Atualizacao de dados das paginas

---
+++ angular/controller/geral-ctrl.js
@@ -0,0 +1,18 @@
+generalSCG.controller("shortcutg", function($scope) {
+	$scope.study = [ {label: 'Deep Learning - Inteligencia Artificial', url: 'http://deeplearningbook.com.br/', img: 'https://www.clinks.com.br/wp-content/uploads/2018/02/relacao-inteligencia-artificial-sistema-cognitivo.png'}
+					];
+	$scope.lineSocial = [	{label: 'Facebook', url: 'https://www.facebook.com/', img: 'https://cdn.pixabay.com/photo/2017/08/20/10/30/facebook-2661207_960_720.jpg'}, 
+							{label: 'Instagram', url: 'https://www.instagram.com/', img: 'https://images.vexels.com/media/users/3/147098/isolated/lists/931095c4442a6b69f717233f80aa0a68-botao-de-foto-do-instagram.png'}, 
+							{label: 'Twitter', url: 'https://twitter.com/', img: 'https://img.icons8.com/nolan/2x/twitter.png'}, 
+							{label: 'Gmail', url: 'https://gmail.com/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjLEZ5e7SHOdhu5oEBfcqZW0lV8CIluwQTxZyxvyBEXp8Lo8LTA'}, 
+							{label: 'Reddit', url: 'https://www.reddit.com', img: 'https://cdn2.iconfinder.com/data/icons/2018-social-media-app-logos/1000/2018_social_media_popular_app_logo_reddit-512.png'}, 
+							{label: 'Linkedin', url: 'https://www.linkedin.com/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1TVPHx0gOK-Zq0wb-VsD4gfpyEENGcQ2PnbrQAIIxL-Kw2XE'}
+						];
+
+	$scope.shortcut = [	{label: 'Biblioteca Digital', url: 'http://gen.lib.rus.ec/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4MfOPQiixF9KfQauqvkIabFWDfgiDjVgWAjWYMWEaGewVnAiNFQ'} 
+						];
+
+	$scope.project = [	{label: 'Lyrics Project', url: 'https://1995.netlify.com/', img: 'https://pbs.twimg.com/profile_images/422137684014792704/GZfNf9ti.png'}, 
+						{label: 'Movies DBZN', url: 'https://moviedbzn.netlify.com/', img: 'http://www.pkicon.com/icons/8384/Dragon-Ball-256.png'} 
+					 ];
+});
---
+++ angular/model/geral-app.js
@@ -0,0 +1,1 @@
+var generalSCG = angular.module("generalSCG", []);
