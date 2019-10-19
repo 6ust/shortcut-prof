48da60cff63467fdc7eb6b6e875ba840ffa5f9da
6ust <gust.guedes95@gmail.com>
2019-10-19 17:51:55 -0300

    inclusao de conteudo: Google Drive, Whatsapp Web, Pinterest, Outlook

--- angular/controller/geral-ctrl.js
+++ angular/controller/geral-ctrl.js
@@ -6,7 +6,11 @@ generalSCG.controller("shortcutg", function($scope) {
 							{label: 'Twitter', url: 'https://twitter.com/', img: 'https://img.icons8.com/nolan/2x/twitter.png'}, 
 							{label: 'Gmail', url: 'https://gmail.com/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjLEZ5e7SHOdhu5oEBfcqZW0lV8CIluwQTxZyxvyBEXp8Lo8LTA'}, 
 							{label: 'Reddit', url: 'https://www.reddit.com', img: 'https://cdn2.iconfinder.com/data/icons/2018-social-media-app-logos/1000/2018_social_media_popular_app_logo_reddit-512.png'}, 
-							{label: 'Linkedin', url: 'https://www.linkedin.com/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1TVPHx0gOK-Zq0wb-VsD4gfpyEENGcQ2PnbrQAIIxL-Kw2XE'}
+							{label: 'Linkedin', url: 'https://www.linkedin.com/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1TVPHx0gOK-Zq0wb-VsD4gfpyEENGcQ2PnbrQAIIxL-Kw2XE'}, 
+							{label: 'Google Drive', url: 'https://drive.google.com', img: 'https://lh3.googleusercontent.com/BjEoTf5LtJenRfAf8Xmmdh3JzbyOEKaerqnRyNcwZ9l1ZQocnJP0h1QvuLelI16PNs9LilHQ=w128-h128-e365'}, 
+							{label: 'Whatsapp Web', url: 'https://www.whatsapp.com/?lang=pt_br', img: 'https://dl1.cbsistatic.com/i/2016/05/11/93d7eae4-8cab-4e76-b4e4-dd932d302bf3/8d7c6881e1f2a951bef40e039e58b587/imgingest-8323235902858999630.png'}, 
+							{label: 'Pinterest', url: 'https://br.pinterest.com/', img: 'http://cinematografico.com.br/wp-content/uploads/2013/09/pinterest.png'}, 
+							{label: 'Outlook', url: 'https://outlook.live.com/owa/', img: 'https://newsignature.com/wp-content/uploads/2019/04/outlook.png'}
 						];
 
 	$scope.shortcut = [	{label: 'Biblioteca Digital', url: 'http://gen.lib.rus.ec/', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4MfOPQiixF9KfQauqvkIabFWDfgiDjVgWAjWYMWEaGewVnAiNFQ'} 
